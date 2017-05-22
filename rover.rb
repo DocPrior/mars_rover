7e3b0b71bc53ab2d67fbf3f2fb49474e3aa06f6c
DocPrior <Sara Prior>
2017-05-22 16:57:54 -0400

    create turn method, start debugging

--- rover.rb
+++ rover.rb
@@ -7,27 +7,53 @@ class Rover
   end
 
   def read_instruction(inst)
-    if (inst = "L") || (inst = "R")
+    if (inst == "L") || (inst == "R")
       turn
     end
 
-    if inst = "M"
+    if inst == "M"
       move
     end
   end
 
-  def move(dir)
-    if dir = "N"
+  def moveS
+    if dir == "N"
       @y += 1
     end
-    if dir = "W"
+    if dir == "W"
       @x -= 1
     end
-    if dir = "E"
+    if dir == "E"
       @x += 1
     end
-    if dir = "S"
+    if dir == "S"
       @y -= 1
+    end
+  end
+
+  def turn 
+    if inst == "L"
+      if @dir == "N"
+        @dir = "W"
+      elsif @dir == "W"
+        @dir = "S"
+      elsif @dir == "S"
+        @dir = "E"
+      else
+        @dir = "N"
+      end
+
+      if inst == "R"
+        if @dir == "N"
+          @dir = "E"
+        elsif @dir == "E"
+          @dir = "S"
+        elsif @dir == "S"
+          @dir = "W"
+        else
+          @dir = "N"
+        end
+      end
     end
   end
 end
